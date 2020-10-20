5bc53595889affe2684249325065e21ee2c75e91
evocert <73160780+evocert@users.noreply.github.com>
2020-10-20 03:00:27 -0700

    test

--- kwe.go
+++ kwe.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func test() {
+	resp, err := http.Get("https://api.github.com/users/tensorflow")
+	if err != nil {
+		print(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+	fmt.Print(string(body))
+}
 func main() {
 	fmt.Print("Hello")
+	test()
 }
