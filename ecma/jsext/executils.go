4cdac0bc9df34cfd97fd06a8df5bc694508170df
skullquake <ockert8080@gmail.com>
2020-11-11 17:44:58 +0200

    jsext executils

--- ecma/jsext/executils.go
+++ ecma/jsext/executils.go
@@ -1,6 +1,7 @@
 package jsext
 import(
 	"github.com/dop251/goja"
+	"os/exec"
 )
 func Register_jsext_executils(vm*goja.Runtime){
 	vm.SetFieldNameMapper(goja.TagFieldNameMapper("json",true))
@@ -14,6 +15,8 @@ func Register_jsext_executils(vm*goja.Runtime){
 	vm.Set("executils",struct{
 		Version Version`json:"version"`
 		About func()(string)`json:"about"`
+		//Exec func(string,...string)(string)`json:"exec"`
+		Exec func(...string)(string)`json:"exec"`
 	}{
 		Version:Version{
 			Major:0,
@@ -22,6 +25,16 @@ func Register_jsext_executils(vm*goja.Runtime){
 		},
 		About:func()(string){
 			return "executils contains various utility functions for process execution"
+		},
+		Exec:func(args...string)(string){
+			cmdpath:=args[0]
+			cmdargs:=args[1:]
+			cmd:=exec.Command(cmdpath,cmdargs...)
+			out,err:=cmd.Output()
+			if err!=nil{
+				panic(vm.ToValue("Execution error"))
+			}
+			return string(out)
 		},
 	})
 }
