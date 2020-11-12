f1e2c60899aac1aac706fb29de5bc2c6d3b70a47
evocert <73160780+evocert@users.noreply.github.com>
2020-11-12 16:15:58 +0200

    Rolled back to default postgres using pgx

--- database/postgres/postgres.go
+++ database/postgres/postgres.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/evocert/kwe/database"
 	//helper registration posgres server pgx driver
-	//_ "github.com/jackc/pgx/v4/stdlib"
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	//_ "github.com/lib/pq"
 )
 
 //Open -wrap sql.Open("pgx", datasource)
 func Open(datasource string) (*sql.DB, error) {
-	return sql.Open("postgres", datasource)
+	return sql.Open("pgx", datasource)
 }
 
 func init() {
