bce196178ddf57b4ff0a8fda620077189a2592b4
evocert <73160780+evocert@users.noreply.github.com>
2020-12-21 20:13:21 +0200

    miner update

--- __debug_bin.conf.js
+++ __debug_bin.conf.js
@@ -1,6 +1,8 @@
 <@ 
 resourcing.RegisterEndpoint("/","D:/projects/system");
 resourcing.MapEndPointResource("/","test-this.html","<h1>test this</h1>");
-println(resourcing.FindRSString("/test-this.html"))
+//println(resourcing.FindRSString("/test-this.html"))
+println(resourcing.RegisteredPaths())
+println(resourcing.RegisteredRootPaths())
 channel.Listener().Listen(":1040");
  @> 
