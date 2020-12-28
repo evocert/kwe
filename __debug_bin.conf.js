22381fbf82f6ffdda28e9b445fa45799173e8fe4
evocert <73160780+evocert@users.noreply.github.com>
2020-12-28 08:35:58 +0200

    miner update

--- __debug_bin.conf.js
+++ __debug_bin.conf.js
@@ -1,9 +1,5 @@
 <@
 import "bla";
-async function f1() {
-    var x = await resolveAfter2Seconds(10);
-    console.log(x); // 10
-  }
 resourcing.RegisterEndpoint("/","D:/projects/system");
 resourcing.MapEndpointResource("/","test-this.html","<h1>test this</h1>");
 //println(resourcing.FindRSString("/test-this.html"))
@@ -13,5 +9,4 @@ println(resourcing.EndpointViaPath("/").Files())
 println(resourcing.RegisteredRootPaths())
 parseEval("println(\"from parseval\");")
 channel.Listener().Listen(":1040");
-
  @> 
