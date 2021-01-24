def2bd08805f216897fd009b50d7ec3c284fc8fd
evocert <73160780+evocert@users.noreply.github.com>
2021-01-24 11:43:11 +0200

    comment out testing of command

--- kwe.go
+++ kwe.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	_ "github.com/evocert/kwe/database/mysql"
 	_ "github.com/evocert/kwe/database/ora"
 	_ "github.com/evocert/kwe/database/postgres"
 	_ "github.com/evocert/kwe/database/sqlserver"
-	"github.com/evocert/kwe/osprc"
 	"github.com/evocert/kwe/service"
 )
 
 func main() {
-	if cmd, cmderr := osprc.NewCommand("cmd", "-"); cmderr == nil {
+	/*if cmd, cmderr := osprc.NewCommand("cmd", "-"); cmderr == nil {
 		for {
 			if rs, _ := cmd.Readln(); rs != "" {
 				fmt.Println(rs)
@@ -47,6 +45,6 @@ func main() {
 		}
 		fmt.Println(cmd.Dir())
 		cmd.Close()
-	}
+	}*/
 	service.RunService(os.Args...)
 }
