43887b42d457faab06cc03cab4d9b71a2bfacc0a
evocert <73160780+evocert@users.noreply.github.com>
2021-01-24 11:09:19 +0200

    updated Command Reader

--- kwe.go
+++ kwe.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	_ "github.com/evocert/kwe/database/mysql"
 	_ "github.com/evocert/kwe/database/ora"
 	_ "github.com/evocert/kwe/database/postgres"
 	_ "github.com/evocert/kwe/database/sqlserver"
+	"github.com/evocert/kwe/osprc"
 	"github.com/evocert/kwe/service"
 )
 
 func main() {
-	/*if cmd, cmderr := osprc.NewCommand("cmd"); cmderr == nil {
+	if cmd, cmderr := osprc.NewCommand("cmd", "-"); cmderr == nil {
 		for {
 			if rs, _ := cmd.Readln(); rs != "" {
 				fmt.Println(rs)
@@ -19,15 +21,32 @@ func main() {
 				break
 			}
 		}
-		cmd.Println("go get -u -v github.com/dop251/goja")
+		cmd.Println("whoami")
 		for {
-			if rs, _ := cmd.Readln(); rs != "" {
+			if rs, _ := cmd.ReadAll(); rs != "" {
+				fmt.Println(rs)
+			} else {
+				break
+			}
+		}
+		cmd.Println("date")
+		for {
+			if rs, _ := cmd.ReadAll(); rs != "" {
+				fmt.Println(rs)
+			} else {
+				break
+			}
+		}
+		cmd.Println("time")
+		for {
+			if rs, _ := cmd.ReadAll(); rs != "" {
 				fmt.Println(rs)
 			} else {
 				break
 			}
 		}
 		fmt.Println(cmd.Dir())
-	}*/
+		cmd.Close()
+	}
 	service.RunService(os.Args...)
 }
--- osprc/command.go
+++ osprc/command.go
@@ -21,7 +21,14 @@ type Command struct {
 	cmdin      io.WriteCloser
 	bfr        *bufio.Reader
 	cmdout     io.ReadCloser
-	cmdoutdne  chan bool
+	cmdoutp    chan []byte
+	cmdouterr  chan error
+	cmdtmpp    []byte
+	cmdtmppi   int
+	cmdtmppl   int
+	stdinpark  []byte
+	stdinparkl int
+	stdinparki int
 	cancmdout  bool
 }
 
@@ -29,12 +36,28 @@ type Command struct {
 func NewCommand(execpath string, execargs ...string) (cmd *Command, err error) {
 	var ctx, ctxcancel = context.WithCancel(context.Background())
 	excmd := exec.CommandContext(ctx, execpath, execargs...)
-
 	if cmdout, cmdouterr := excmd.StdoutPipe(); cmdouterr == nil {
 		if cmdin, cmdinerr := excmd.StdinPipe(); cmdinerr == nil {
 			if err = excmd.Start(); err == nil {
-				cmd = &Command{excmd: excmd, excmdprcid: -1, OnClose: nil, ctx: ctx, ctxcancel: ctxcancel, cmdin: cmdin, cancmdout: false, cmdoutdne: make(chan bool, 1), cmdout: cmdout}
+				cmd = &Command{excmd: excmd, excmdprcid: -1, OnClose: nil, ctx: ctx, ctxcancel: ctxcancel, cmdin: cmdin, cancmdout: false, cmdtmpp: make([]byte, 1024), stdinparkl: 0, stdinparki: 0, stdinpark: make([]byte, 1024), cmdtmppi: 0, cmdtmppl: 0, cmdoutp: make(chan []byte, 1), cmdouterr: make(chan error, 1), cmdout: cmdout}
 				cmd.excmdprcid = excmd.Process.Pid
+				go func() {
+					p := make([]byte, 1024)
+					n := 0
+					err := error(nil)
+					for {
+						n, err = cmd.cmdout.Read(p)
+						var bts []byte = make([]byte, n)
+						if n > 0 {
+							copy(bts, p)
+						}
+						cmd.cmdoutp <- bts
+						cmd.cmdouterr <- err
+						if err != nil && err != io.EOF {
+							break
+						}
+					}
+				}()
 			} else {
 				cmdin = nil
 				cmdout = nil
@@ -141,6 +164,14 @@ func (cmd *Command) Close() (err error) {
 			}
 			cmd.excmd = nil
 		}
+		if cmd.cmdoutp != nil {
+			close(cmd.cmdoutp)
+			cmd.cmdoutp = nil
+		}
+		if cmd.cmdouterr != nil {
+			close(cmd.cmdouterr)
+			cmd.cmdouterr = nil
+		}
 		if cmd.bfr != nil {
 			cmd.bfr = nil
 		}
@@ -173,16 +204,100 @@ func (cmd *Command) Dir() string {
 //Read - refer to io.Reader
 func (cmd *Command) Read(p []byte) (n int, err error) {
 	if pl := len(p); pl > 0 {
-		if !cmd.cancmdout {
-			go func() {
-				n, err = cmd.cmdout.Read(p)
-				cmd.cancmdout = false
-				cmd.cmdoutdne <- true
-			}()
+		lststderr := error(nil)
+		for n < pl {
+			if cmd.cmdtmppl == 0 || (cmd.cmdtmppl > 0 && cmd.cmdtmppl == cmd.cmdtmppi) {
+				if cmd.cmdtmppi > 0 {
+					cmd.cmdtmppi = 0
+				}
+				if cmd.cmdtmppl > 0 {
+					cmd.cmdtmppl = 0
+				}
+				canCapture := true
+				for canCapture {
+					if cmd.stdinparkl == 0 || (cmd.stdinparkl > 0 && cmd.stdinparki == cmd.stdinparkl) {
+						if cmd.stdinparki > 0 {
+							cmd.stdinparki = 0
+						}
+						if cmd.stdinparkl > 0 {
+							cmd.stdinparkl = 0
+						}
+						select {
+						case stdin, ok := <-cmd.cmdoutp:
+							if !ok {
+								canCapture = false
+							} else {
+								lststderr = <-cmd.cmdouterr
+								if cmd.stdinparkl = len(stdin); cmd.stdinparkl > 0 {
+									cmd.stdinparkl = copy(cmd.stdinpark[:cmd.stdinparkl], stdin)
+									canCapture = true
+								} else {
+									canCapture = false
+								}
+							}
+						case <-time.After(1 * time.Second):
+							canCapture = false
+						}
+					}
+					if canCapture {
+						for {
+							if cmdl := len(cmd.cmdtmpp); cmd.cmdtmppi < cmdl {
+								if cl := (cmd.stdinparkl - cmd.stdinparki); cl <= (cmdl - cmd.cmdtmppi) {
+									copy(cmd.cmdtmpp[cmd.cmdtmppi:cmd.cmdtmppi+cl], cmd.stdinpark[cmd.stdinparki:cmd.stdinparki+cl])
+									cmd.cmdtmppl += cl
+									cmd.cmdtmppi += cl
+									cmd.stdinparki += cl
+								} else if cl := (cmdl - cmd.cmdtmppi); cl < (cmd.stdinparkl - cmd.stdinparki) {
+									copy(cmd.cmdtmpp[cmd.cmdtmppi:cmd.cmdtmppi+cl], cmd.stdinpark[cmd.stdinparki:cmd.stdinparki+cl])
+									cmd.cmdtmppl += cl
+									cmd.cmdtmppi += cl
+									cmd.stdinparki += cl
+								}
+								if cmdl == cmd.cmdtmppi {
+									canCapture = false
+									break
+								}
+								if cmd.stdinparki == cmd.stdinparkl {
+									break
+								}
+							}
+						}
+					}
+				}
+				if cmd.cmdtmppl == 0 {
+					break
+				} else {
+					cmd.cmdtmppi = 0
+				}
+			}
+			for n < pl && cmd.cmdtmppi < cmd.cmdtmppl {
+				if cl := (cmd.cmdtmppl - cmd.cmdtmppi); cl <= (pl - n) {
+					copy(p[n:n+cl], cmd.cmdtmpp[cmd.cmdtmppi:cmd.cmdtmppi+cl])
+					cmd.cmdtmppi += cl
+					n += cl
+				} else if cl := (pl - n); cl < (cmd.cmdtmppl - cmd.cmdtmppi) {
+					copy(p[n:n+cl], cmd.cmdtmpp[cmd.cmdtmppi:cmd.cmdtmppi+cl])
+					cmd.cmdtmppi += cl
+					n += cl
+				}
+			}
+			if cmd.cmdtmppi == cmd.cmdtmppl {
+				break
+			}
 		}
-		select {
-		case <-cmd.cmdoutdne:
-		case <-time.After(500 * time.Millisecond):
+		//if !cmd.cancmdout {
+		//	go func() {
+		//n, err = cmd.cmdout.Read(p)
+		//		cmd.cancmdout = false
+		//		cmd.cmdoutdne <- true
+		//	}()
+		//}
+		//select {
+		//case <-cmd.cmdoutdne:
+		//case <-time.After(500 * time.Millisecond):
+		//}
+		if lststderr != nil && lststderr != io.EOF {
+
 		}
 		if n == 0 && err == nil {
 			err = io.EOF
