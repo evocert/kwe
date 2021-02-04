22a3d6d0247ebcf5b66f3d2a1d6ca08393018a09
skullquake <ockert8080@gmail.com>
2021-02-04 15:59:26 +0200

    osutils

---
+++ ecma/jsext/osutils.go
@@ -0,0 +1,28 @@
+package jsext
+import (
+	"runtime"
+	"github.com/dop251/goja"
+)
+func Register_jsext_osutils(vm *goja.Runtime) {
+	//vm.SetFieldNameMapper(goja.TagFieldNameMapper("json",true))
+	type Version struct {
+		Major int `json:"major"`
+		Minor int `json:"minor"`
+		Bump  int `json:"bump"`
+	}
+	//todo: namespace everything kwe.fsutils.etcetcetc
+	//first test for kwe then do set fsutils on kwe
+	vm.Set("osutils", struct {
+		Version         Version                  `json:"version"`
+		GOOS            func()string             `json:"GOOS"`
+	}{
+		Version: Version{
+			Major: 0,
+			Minor: 0,
+			Bump:  1,
+		},
+		GOOS: func() string {
+			return runtime.GOOS
+		},
+	})
+}
