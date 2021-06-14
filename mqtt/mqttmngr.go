63a475d1f3a89e6a04526d4e4d2614ea4c109030
evocert <73160780+evocert@users.noreply.github.com>
2021-06-14 15:22:19 +0200

    miner update

--- mqtt/mqttmngr.go
+++ mqtt/mqttmngr.go
@@ -109,10 +109,12 @@ func (mqttmngr *MQTTManager) messageReceived(mqttcn *MQTTConnection, alias strin
 			defer mqttmngr.lcktpcs.RUnlock()
 			atvtpc = mqttmngr.activeTopics[msg.Topic()]
 		}()
-		if atvtpc != nil {
-			msg.tokenpath = atvtpc.topicpath
-			atvtpc.processMessage(mqttmngr.MqttMessaging, msg)
-		}
+		go func() {
+			if atvtpc != nil {
+				msg.tokenpath = atvtpc.topicpath
+				atvtpc.processMessage(mqttmngr.MqttMessaging, msg)
+			}
+		}()
 	}
 }
 
