63fed8ca8203e9c322546ee778a51e856cd12cdb
evocert <73160780+evocert@users.noreply.github.com>
2021-07-08 20:12:31 +0200

    improve logging to console

--- ecma/jsext/consoleutils.go
+++ ecma/jsext/consoleutils.go
@@ -1,7 +1,12 @@
 package jsext
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"sync"
+	"time"
 
 	"github.com/dop251/goja"
 	"github.com/evocert/kwe/iorw"
@@ -37,35 +42,94 @@ func Register_jsext_consoleutils(vm *goja.Runtime) {
 			lgmsg := buf.String()
 			buf.Close()
 			buf = nil
-			log.Println("LOG:   ", lgmsg)
+			//log.Println("LOG:   ", lgmsg)
+			logger.Output(2, fmt.Sprintln("LOG:   "+lgmsg))
 		},
 		Warn: func(msg ...interface{}) {
 			buf := iorw.NewBuffer()
 			buf.Print(msg...)
 			lgmsg := buf.String()
 			buf = nil
-			log.Println("WARN:  ", lgmsg)
+			//log.Println("WARN:  ", lgmsg)
+			logger.Output(2, fmt.Sprintln("WARN:   "+lgmsg))
 		},
 		Error: func(msg ...interface{}) {
 			buf := iorw.NewBuffer()
 			buf.Print(msg...)
 			lgmsg := buf.String()
 			buf = nil
-			log.Println("ERROR: ", lgmsg)
+			//log.Println("ERROR: ", lgmsg)
+			logger.Output(2, fmt.Sprintln("ERROR:   "+lgmsg))
 		},
 		Debug: func(msg ...interface{}) {
 			buf := iorw.NewBuffer()
 			buf.Print(msg...)
 			lgmsg := buf.String()
 			buf = nil
-			log.Println("DEBUG: ", lgmsg)
+			//log.Println("DEBUG: ", lgmsg)
+			logger.Output(2, fmt.Sprintln("DEBUG:   "+lgmsg))
 		},
 		Trace: func(msg ...interface{}) {
 			buf := iorw.NewBuffer()
 			buf.Print(msg...)
 			lgmsg := buf.String()
 			buf = nil
-			log.Println("TRACE: ", lgmsg)
+			//log.Println("TRACE: ", lgmsg)
+			logger.Output(2, fmt.Sprintln("RACE:   "+lgmsg))
 		},
 	})
+}
+
+var logger *log.Logger = nil
+
+type readwrite struct {
+	inbf     *iorw.Buffer
+	outw     io.Writer
+	lckinout *sync.RWMutex
+	flshdr   time.Duration
+}
+
+func (rw *readwrite) Write(p []byte) (n int, err error) {
+	if pl := len(p); pl > 0 {
+		func() {
+			rw.lckinout.RLock()
+			defer rw.lckinout.RUnlock()
+			n, err = rw.inbf.Write(p[:pl])
+		}()
+	}
+	return
+}
+
+func (rw *readwrite) ticFlushing() {
+	var checksize = func() int64 {
+		rw.lckinout.RLock()
+		defer rw.lckinout.RUnlock()
+		return rw.inbf.Size()
+	}
+	var bufout = iorw.NewBuffer()
+	for {
+		time.Sleep(rw.flshdr)
+		if checksize() > 0 {
+			func() {
+				rw.lckinout.Lock()
+				defer rw.lckinout.Unlock()
+				rd := rw.inbf.Reader()
+				io.Copy(bufout, rd)
+				rd.Close()
+				rw.inbf.Clear()
+			}()
+			if bufout.Size() > 0 {
+				rd := bufout.Reader()
+				io.Copy(rw.outw, rd)
+				rd.Close()
+				bufout.Clear()
+			}
+		}
+	}
+}
+
+func init() {
+	var rw = &readwrite{inbf: iorw.NewBuffer(), lckinout: &sync.RWMutex{}, flshdr: time.Millisecond * 500, outw: os.Stderr}
+	go rw.ticFlushing()
+	logger = log.New(rw, "", log.LstdFlags)
 }
