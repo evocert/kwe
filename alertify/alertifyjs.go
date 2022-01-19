0120fa19679a89f5bd8a19214c5a7f67c4a23989
evocert <73160780+evocert@users.noreply.github.com>
2022-01-19 11:36:01 +0200

    moved /alertify/html/head.html to /alertify/head.html

--- alertify/alertifyjs.go
+++ alertify/alertifyjs.go
@@ -1,7 +1,6 @@
 package alertify
 
 import (
-	"io"
 	"strings"
 
 	_ "embed"
@@ -15,24 +14,16 @@ var alertifyjs string
 //go:embed css/alertify.min.css
 var alertifycss string
 
-func AlertifyJS() io.Reader {
-	return strings.NewReader(alertifyjs)
-}
-
-func AlertifyCSS() io.Reader {
-	return strings.NewReader(alertifycss)
-}
-
 const alertifyheadhtml string = `<link rel="stylesheet" href="/alertify/css/alertify.css">
 <script type="application/javascript" src="/alertify/js/alertify.js"></script>`
 
 func init() {
 	gblrs := resources.GLOBALRSNG()
-	gblrs.FS().MKDIR("/raw:alertify/html", "")
+	gblrs.FS().MKDIR("/raw:alertify", "")
 	gblrs.FS().MKDIR("/raw:alertify/css", "")
 	gblrs.FS().MKDIR("/raw:alertify/js", "")
 	gblrs.FS().SET("/alertify/css/alertify.css", alertifycss)
 	gblrs.FS().SET("/alertify/js/alertify.js", alertifyjs)
 
-	gblrs.FS().SET("/alertify/html/head.html", strings.NewReader(alertifyheadhtml))
+	gblrs.FS().SET("/alertify/head.html", strings.NewReader(alertifyheadhtml))
 }
