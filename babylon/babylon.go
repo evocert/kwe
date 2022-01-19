15ba070854a192ee03c7174e041d34eda65c54f1
evocert <73160780+evocert@users.noreply.github.com>
2022-01-19 17:19:13 +0200

    cleanup and move baylons.js to js/babylon

--- babylon/babylon.go
+++ babylon/babylon.go
@@ -2,7 +2,18 @@ package babylon
 
 import (
 	_ "embed"
+
+	"github.com/evocert/kwe/resources"
 )
 
-//go:embed babylon.js
+//go:embed js/babylon.js
 var babylonjs string
+
+func init() {
+	rsmngrfs := resources.GLOBALRSNG().FS()
+	rsmngrfs.MKDIR("/raw:babylon/js", "")
+	rsmngrfs.SET("/babylon/js/babylon.js", babylonjs)
+	rsmngrfs.MKDIR("/raw:babylon", "")
+	rsmngrfs.SET("/babylon/head.html",
+		`<script type="application/javascript" src="/babylon/js/babylon.js"></script>`)
+}
