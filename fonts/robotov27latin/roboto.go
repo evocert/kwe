1f4e683f712e10df4b1c61cc438eb46628673e89
evocert <73160780+evocert@users.noreply.github.com>
2022-01-25 15:11:28 +0200

    moved to /roboto/head.html

--- fonts/robotov27latin/roboto.go
+++ fonts/robotov27latin/roboto.go
@@ -19,6 +19,6 @@ func init() {
 	gblrs.FS().MKDIR("/raw:roboto/fonts", "")
 	gblrs.FS().SET("/roboto/css/index.css", indexcss)
 	gblrs.FS().SET("/roboto/fonts/roboto-v27-latin-regular.woff", bytes.NewReader(roboto_v27_latin_regular_woff))
-	gblrs.FS().MKDIR("/raw:roboto/html", "")
-	gblrs.FS().SET("/roboto/html/head.html", `<link rel="stylesheet" type="text/css" href="/roboto/css/index.css">`)
+	gblrs.FS().MKDIR("/raw:roboto", "")
+	gblrs.FS().SET("/roboto/head.html", `<link rel="stylesheet" type="text/css" href="/roboto/css/index.css">`)
 }
