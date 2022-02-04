ebc12e11797a4b25fd40c830eb3e9f6d96d855f4
evocert <73160780+evocert@users.noreply.github.com>
2022-02-04 13:01:06 +0200

    moved /webactions/head.html

--- webactions/webactions.go
+++ webactions/webactions.go
@@ -41,6 +41,5 @@ func init() {
 	gblrs.FS().SET("/webactions/webactions-core.js", WebactionsJS())
 	gblrs.FS().SET("/webactions/webactions.js", WebactionsBundleJS())
 	gblrs.FS().SET("/webactions/webactions.min.js", WebactionsBundleJS())
-	gblrs.FS().MKDIR("/raw:webactions/html", "")
-	gblrs.FS().SET("/webactions/html/head.html", `<script type="application/javascript" src="/webactions/webactions.min.js"></script>`)
+	gblrs.FS().SET("/webactions/head.html", `<script type="application/javascript" src="/webactions/webactions.min.js"></script>`)
 }
