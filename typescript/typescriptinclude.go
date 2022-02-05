6bd78ae08ba4e2edf16bca3c125aff7c55c575a8
evocert <73160780+evocert@users.noreply.github.com>
2022-02-05 07:02:33 +0200

    remove embedded typescript.js resource

--- typescript/typescriptinclude.go
+++
@@ -1,9 +0,0 @@
-package typescript
-
-import "github.com/evocert/kwe/resources"
-
-func init() {
-	gblrs := resources.GLOBALRSNG()
-	gblrs.FS().MKDIR("/raw:typescript", "")
-	gblrs.FS().SET("/typescript/typescript.js", TypescriptJS())
-}
