4fd54e03e3b96b9461d47ec7d0490e21f471cd03
Evert Joubert <evocert@gmail.com>
2022-04-04 11:11:52 +0200

    roll back

--- database/ora/ora.go
+++ database/ora/ora.go
@@ -7,12 +7,9 @@ import (
 	"github.com/evocert/kwe/database"
 	//helper registration oracle server driver
 	_ "github.com/sijms/go-ora"
-	v2 "github.com/sijms/go-ora/v2"
 )
 
 //Open -wrap sql.Open("oracle", datasource)
-//or
-//Open -wrap sql.Open("oracle-ext", datasource)
 func Open(oraname, datasource string) (*sql.DB, error) {
 	if url, _ := url.ParseRequestURI(datasource); url != nil {
 		return sql.Open(oraname, datasource)
@@ -21,13 +18,8 @@ func Open(oraname, datasource string) (*sql.DB, error) {
 }
 
 func init() {
-	sql.Register("oracle-xe", &v2.OracleDriver{})
 	database.GLOBALDBMS().RegisterDriver("oracle", func(datasource string, a ...interface{}) (db *sql.DB, err error) {
 		db, err = Open("oracle", datasource)
-		return
-	})
-	database.GLOBALDBMS().RegisterDriver("oracle-ext", func(datasource string, a ...interface{}) (db *sql.DB, err error) {
-		db, err = Open("oracle-ext", datasource)
 		return
 	})
 }
